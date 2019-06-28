a642fb85b853d864ca5fd1180a018dcaa3164926
glissa <Guillin>
2019-06-28 02:58:20 +0000

    Se creo proyecto en el repositorio. Competencia ECI 2019

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Competencia patrocinada por fundacion Sadosky y AlixPatners',
+    author='Guillin',
+    license='MIT',
+)
